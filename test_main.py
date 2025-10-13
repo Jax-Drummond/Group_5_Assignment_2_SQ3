fbe387ed97dc6cd62b3e70f3eaccba5de854fec9
Harris <moejoe494@gmail.com>
2025-10-13 17:01:58 -0400

    Fixed test_main based on pylint suggestions
    - Modified test_main to be in line with what pylint suggested
    - Changed pylint settings to detect indents as tabs instead of spaces

--- test_main.py
+++ test_main.py
@@ -1,9 +1,9 @@
-import numpy as np
-from PIL import Image
 import sys
 import os
 import io
 import pytest
+import numpy as np
+from PIL import Image
 from model import preprocess_img, predict_result
 from app import app
 
@@ -17,34 +17,34 @@ def client():
 def test_home_page_gives_status_code_200(client):
 	response = client.get('/')
 	assert response.status_code == 200
-	
+
 def test_home_page_has_title(client):
 	response = client.get('/')
 	assert b"Hand Sign Digit Language Detection" in response.data
-	
+
 def test_home_page_has_description(client):
 	response = client.get('/')
 	assert b"A webapp to detect a digit using hand sign language." in response.data
 
 def test_prediction_with_valid_file_shows_prediction(client):
 	image = Image.open("test_images/0/Sign 0 (21).jpeg")
-	bytes = io.BytesIO()
-	image.save(bytes, format="JPEG")
+	image_bytes = io.BytesIO()
+	image.save(image_bytes, format="JPEG")
 
-	imgData = {
-			"file": (bytes.getvalue(), "test.png")
+	image_data = {
+			"file": (image_bytes.getvalue(), "test.png")
 	}
 
-	response = client.post("/prediction", content_type="multipart/form-data", data=imgData)
+	response = client.post("/prediction", content_type="multipart/form-data", data=image_data)
 	assert b"Prediction" in response.data
 
 def test_prediction_with_invalid_file_shows_error_messages(client):
-	txtFile = io.BytesIO(b"")
-	txtData = {
-			"file": (txtFile, "blank.txt")
+	blank_file = io.BytesIO(b"")
+	blank_data = {
+			"file": (blank_file, "blank.txt")
 	}
 
-	response = client.post("/prediction", content_type="multipart/form-data", data=txtData)
+	response = client.post("/prediction", content_type="multipart/form-data", data=blank_data)
 	assert response.status_code == 200
 	assert b"File cannot be processed." in response.data
 
